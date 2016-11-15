5472dae71cd70c63a514864d41e49d630be90e74
pnhung177 <pnhung177@acegik.net>
2016-11-15 21:27:23 +0700

    Append the demo pages

--- test/app/config/sandbox_test.js
+++ test/app/config/sandbox_test.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: {
-    appSidebar: {},
+    appSidebar: {
+      interceptUrls: []
+    },
     appWebserver: {}
   }
 };
---
+++ test/app/lib/services/sidebar-example.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var events = require('events');
+var util = require('util');
+var fs = require('fs');
+var path = require('path');
+var Devebot = require('devebot');
+var Promise = Devebot.require('bluebird');
+var lodash = Devebot.require('lodash');
+var debug = Devebot.require('debug');
+var debuglog = debug('appSidebar:example');
+
+var Service = function(params) {
+  debuglog.isEnabled && debuglog(' + constructor begin ...');
+
+  params = params || {};
+
+  var self = this;
+
+  self.logger = params.loggingFactory.getLogger();
+
+  self.getSandboxName = function() {
+    return params.sandboxName;
+  };
+
+  var pluginCfg = lodash.get(params, ['sandboxConfig', 'plugins', 'appSidebar'], {});
+  debuglog.isEnabled && debuglog(' - appSidebar config: %s', JSON.stringify(pluginCfg));
+
+  var contextPath = pluginCfg.contextPath || '/sidebar';
+  var webserverTrigger = params.webserverTrigger;
+  var express = webserverTrigger.getExpress();
+  var position = webserverTrigger.getPosition();
+
+  webserverTrigger.inject(express.static(path.join(__dirname, '../../public')),
+      contextPath, position.inRangeOfStaticFiles(), 'app-sidebar-public');
+
+  self.getServiceInfo = function() {
+    return {};
+  };
+
+  self.getServiceHelp = function() {
+    return {};
+  };
+
+  debuglog.isEnabled && debuglog(' - constructor end!');
+};
+
+Service.argumentSchema = {
+  "id": "sidebarExample",
+  "type": "object",
+  "properties": {
+    "sandboxName": {
+      "type": "string"
+    },
+    "sandboxConfig": {
+      "type": "object"
+    },
+    "profileConfig": {
+      "type": "object"
+    },
+    "generalConfig": {
+      "type": "object"
+    },
+    "loggingFactory": {
+      "type": "object"
+    },
+    "webserverTrigger": {
+      "type": "object"
+    }
+  }
+};
+
+module.exports = Service;
